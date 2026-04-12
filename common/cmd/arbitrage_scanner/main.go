b067224d8c1e162da689230685ab55a1713c0693
krxzy37 <pala44archuk@gmail.com>
2026-04-12 21:10:04 +0300

    ready raw info about bids and asks

--- common/cmd/arbitrage_scanner/main.go
+++ common/cmd/arbitrage_scanner/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
 	internal "github.com/krzy37/arbitrage-scanner/common/internal/connect"
-	shared "github.com/krzy37/arbitrage-scanner/common/pkg/logger"
 )
 
 func main() {
+	/*
+		cfg := shared.NewConfigMust()
+		logger, err := shared.NewLogger(cfg)
+		if err != nil {
+			panic(fmt.Sprintf("error creating logger: %v", err))
+		}
+		defer logger.Close()
+		defer func() {
+			_ = logger.Sync()
+		}()
 
-	cfg := shared.NewConfigMust()
-	logger, err := shared.NewLogger(cfg)
-	if err != nil {
-		panic(fmt.Sprintf("error creating logger: %v", err))
-	}
-	defer logger.Close()
-	defer func() {
-		_ = logger.Sync()
-	}()
-
-	logger.Info("logger init success")
-
+		logger.Info("logger init success")
+	*/
 	internal.BinanceConnect()
+
+	time.Sleep(time.Duration(5) * time.Second)
 }
--- common/internal/connect/conBinance.go
+++ common/internal/connect/conBinance.go
@@ -3,12 +3,49 @@ package internal
 import (
 	"fmt"
 
+	"github.com/VictorLowther/btree"
 	"github.com/gorilla/websocket"
 )
 
-const (
-	wsendpoint = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth"
-)
+const wsendpoint = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth"
+
+func byBestBid(a, b *OrderBookEntry) bool {
+	return a.Price >= b.Price
+}
+func byBestAsk(a, b *OrderBookEntry) bool {
+	return a.Price < b.Price
+}
+
+type OrderBookEntry struct {
+	Price  float64
+	Volume float64
+}
+type OrderBook struct {
+	Asks *btree.Tree[*OrderBookEntry]
+	Bids *btree.Tree[*OrderBookEntry]
+}
+
+func NewOrderBook() *OrderBook {
+	return &OrderBook{
+		Asks: btree.New(byBestAsk),
+		Bids: btree.New(byBestBid),
+	}
+}
+
+func (ob *OrderBook) handleDepthResponse(res BinanceDepthResult) {
+	for _, ask := range res.Asks {
+		fmt.Println(ask)
+	}
+}
+
+type BinanceDepthResult struct {
+	Asks [][]string `json:"a"`
+	Bids [][]string `json:"b"`
+}
+type BinanceDepthResponse struct {
+	Stream string             `json:"stream"`
+	Data   BinanceDepthResult `json:"data"`
+}
 
 func BinanceConnect() {
 	conn, _, err := websocket.DefaultDialer.Dial(wsendpoint, nil)
@@ -17,5 +54,15 @@ func BinanceConnect() {
 		panic(err)
 	}
 
-	fmt.Println(conn)
+	var (
+		ob     = NewOrderBook()
+		result BinanceDepthResponse
+	)
+	for {
+		if err := conn.ReadJSON(&result); err != nil {
+			panic(err)
+		}
+
+		ob.handleDepthResponse(result.Data)
+	}
 }
